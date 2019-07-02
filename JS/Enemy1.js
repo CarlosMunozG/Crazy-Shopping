7c896f0404c3a273bf039263fdd1727e84b39382
Carlos Muñoz <carlosmunyozguimera@gmail.com>
2019-07-02 15:44:00 +0200

    eliminate extra files

--- JS/Enemy1.js
+++ JS/Enemy1.js
@@ -1,1 +1,22 @@
 'use strict'
+
+function Enemy1(canvas, randomX) {
+  this.canvas = canvas;
+  this.ctx = canvas.getContext('2d');
+  this.height = 20;
+  this.width = 20;
+  this.x = randomX;
+  this.y = this.canvas.height;
+  this.velocity = 3;
+  this.direction = -1;
+  this.color = 'red';
+}
+
+/*Enemy.prototype.move = function() {
+  this.x = this.x + (this.direction * this.velocity);
+}
+
+Enemy.prototype.draw = function() {
+  this.ctx.fillStyle = this.color;
+  this.ctx.fillRect( this.x, this.y, this.width, this.height);
+}*/
